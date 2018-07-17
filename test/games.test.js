bddd9f1a1f9b9fd375b5d700c138de9948bd061f
MariahAdams <mariah.adams23@yahoo.com>
2018-07-16 17:31:26 -0700

    setting up tests for games

---
+++ test/games.test.js
@@ -0,0 +1,10 @@
+const mongo = require('../lib/mongodb');
+const { assert } = require('chai');
+const request = require('./request');
+
+describe('Games API', () => {
+
+    it('working...', () => {
+        //wip
+    });
+});
