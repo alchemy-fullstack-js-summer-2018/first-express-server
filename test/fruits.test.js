471685d41c787a69abd86cf364870140f2b39e21
Kevin Grayson <k.grayson.ms@gmail.com>
2018-07-17 09:24:44 -0700

    Saves a fruit working, setting up gets a fruit

--- test/fruits.test.js
+++ test/fruits.test.js
@@ -4,8 +4,46 @@ const request = require('./request');
 
 describe('Fruits API', () => {
 
+    beforeEach(() => {
+        return mongo.then(db => {
+            return db.collection('fruits').remove();
+        });
+    });
+
+    function save(fruit) {
+        return request
+            .post('/api/fruits')
+            .send(fruit)
+            .then(( {body }) => body);
+    }
+
+    let apple;
+
+    beforeEach(() => {
+        return save({
+            type: 'Apple',
+            color: 'Red',
+            price: '$0.39'
+        })
+            .then(data => {
+                apple = data;
+            });
+    })
+
     it('Wired up and working', () => {
         assert.isOk(true)
+    });
+
+    it('Saves a fruit', () => {
+        assert.isOk(apple._id);
+    });
+
+    it('Gets a fruit', () => {
+        return request
+            .get(`/api/fruits/${apple._id}`)
+            .then(({ body }) => {
+                assert.deepEqual(body, apple)
+            });
     });
 
 });
