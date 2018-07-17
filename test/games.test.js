1a2b7e9aa47c649480528402df9478b93c4706a6
MariahAdams <mariah.adams23@yahoo.com>
2018-07-16 17:36:21 -0700

    setting up test to save game

--- test/games.test.js
+++ test/games.test.js
@@ -4,7 +4,29 @@ const request = require('./request');
 
 describe('Games API', () => {
 
-    it('working...', () => {
-        //wip
+    beforeEach(() => {
+        return mongo.then(db => {
+            return db.collection('games').remove();
+        });
+    });
+
+    function save(game) {
+        return request
+            .post('/api/games')
+            .send(game)
+            .then(({ body }) => body);
+    }
+
+    let botw;
+
+    beforeEach(() => {
+        return save({ name: 'Breath of the Wild' })
+            .then(data => {
+                botw = data;
+            });
+    });
+
+    it('saves a game', () => {
+        assert.isOk(botw._id);
     });
 });
